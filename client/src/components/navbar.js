38ad89f2f6a93a4d202718b762cf51f62bccb79e
Hayden Mak <haydenmlh@gmail.com>
2022-02-05 20:13:50 -0500

    remove bootstrap elements on navbar

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -12,43 +12,29 @@ import img from '../img/RestorEase_transparent.png'
 // Here, we display our Navbar
 export default function Navbar() {
   return (
-    <div>
-      <nav className="navbar navbar-expand-lg navbar-light">
+    <div className="nav_div">
         <NavLink className="navbar-brand" to="/">
           <div className="logo_button">
-            <img src={img} style={{"width": 75}}></img>
+            <img src={img} style={{"width": 75}} alt="logo"></img>
             <h2 className="logo_text">RestorEase</h2>
           </div>
           
         {/* <img style={{"width" : 25 + '%'}} src="https://d3cy9zhslanhfa.cloudfront.net/media/3800C044-6298-4575-A05D5C6B7623EE37/4B45D0EC-3482-4759-82DA37D8EA07D229/webimage-8A27671A-8A53-45DC-89D7BF8537F15A0D.png"></img> */}
         </NavLink>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
+        
   
-        <div className="collapse navbar-collapse links" id="navbarSupportedContent">
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item item">
+        <div className="navbar_buttons">
+            <div className="nav-item">
               <NavLink className="nav-link" to="/about">
                 About
               </NavLink>
-            </li>
-            <li>
-              <NavLink className="nav-link item book" to="/create">
+            </div>
+            <div>
+              <NavLink className="nav-link book" to="/create">
                 Book Now!
               </NavLink>
-            </li>
-          </ul>
+            </div>
         </div>
-      </nav>
     </div>
   );
 }
