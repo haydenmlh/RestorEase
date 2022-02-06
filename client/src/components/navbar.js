3543997857243bfeb4e257983356bc4002609188
Hayden Mak <haydenmlh@gmail.com>
2022-02-05 19:57:21 -0500

    Add logo to navbar

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,11 +1,13 @@
 import React from "react";
-import './component_styles.css'
+import './component_styles.css';
  
 // We import bootstrap to make our application look better.
 import "bootstrap/dist/css/bootstrap.css";
  
 // We import NavLink to utilize the react router.
 import { NavLink } from "react-router-dom";
+
+import img from '../img/RestorEase_transparent.png'
  
 // Here, we display our Navbar
 export default function Navbar() {
@@ -13,7 +15,11 @@ export default function Navbar() {
     <div>
       <nav className="navbar navbar-expand-lg navbar-light">
         <NavLink className="navbar-brand" to="/">
-          <h2 className="logo">RestorEase</h2>
+          <div className="logo_button">
+            <img src={img} style={{"width": 75}}></img>
+            <h2 className="logo_text">RestorEase</h2>
+          </div>
+          
         {/* <img style={{"width" : 25 + '%'}} src="https://d3cy9zhslanhfa.cloudfront.net/media/3800C044-6298-4575-A05D5C6B7623EE37/4B45D0EC-3482-4759-82DA37D8EA07D229/webimage-8A27671A-8A53-45DC-89D7BF8537F15A0D.png"></img> */}
         </NavLink>
         <button
